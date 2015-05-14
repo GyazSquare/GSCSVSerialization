86636dccf47dfef1eace4fc7b54bebdc8c7e4a5b
Goichi Hirakawa <gooichi@gyazsquare.com>
2015-05-14 11:41:53 +0900

    Initial Commit

---
+++ GSCSVSerialization/GSCSVSerialization.h
@@ -0,0 +1,13 @@
+//
+//  GSCSVSerialization.h
+//  GSCSVSerialization
+//
+//  Created by 平川剛一 on 2015/05/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GSCSVSerialization : NSObject
+
+@end
---
+++ GSCSVSerialization/GSCSVSerialization.m
@@ -0,0 +1,13 @@
+//
+//  GSCSVSerialization.m
+//  GSCSVSerialization
+//
+//  Created by 平川剛一 on 2015/05/14.
+//
+//
+
+#import "GSCSVSerialization.h"
+
+@implementation GSCSVSerialization
+
+@end
